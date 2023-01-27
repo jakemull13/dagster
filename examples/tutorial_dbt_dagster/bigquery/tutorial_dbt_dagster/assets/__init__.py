2b253f7c24b342d33df943dd8e14a3dfe08f58bc
jakemull13 <jakemull13@gmail.com>
2023-01-26 22:00:12 -0500

    init dbt project, update projecy paths in the project env variables

--- examples/tutorial_dbt_dagster/bigquery/tutorial_dbt_dagster/assets/__init__.py
+++ examples/tutorial_dbt_dagster/bigquery/tutorial_dbt_dagster/assets/__init__.py
@@ -4,8 +4,8 @@ from dagster import AssetIn, MetadataValue, asset, file_relative_path
 from dagster_dbt import load_assets_from_dbt_project
 
 
-DBT_PROJECT_PATH = file_relative_path(__file__, "../../jaffle_shop")
-DBT_PROFILES = file_relative_path(__file__, "../../jaffle_shop/config")
+DBT_PROJECT_PATH = file_relative_path(__file__, "../../my_dbt_project")
+DBT_PROFILES = file_relative_path(__file__, "../../my_dbt_project/config")
 
 # if larger project use load_assets_from_dbt_manifest
 # dbt_assets = load_assets_from_dbt_manifest(json.load(DBT_PROJECT_PATH + "manifest.json", encoding="utf8"))
